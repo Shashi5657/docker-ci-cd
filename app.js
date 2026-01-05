2c58e288d6f6be0559a13ae7ea05ce71df8452ee
shashi5657 <sangepushashidhar@gmail.com>
2026-01-06 00:17:51 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+import e from "express";
+
+const app = e();
+
+app.get("/", (req, res) => {
+  res.send("🚀 Hello World..!!");
+});
+
+app.listen(8001, "0.0.0.0", () => {
+  console.log("Server running on 8001");
+});
